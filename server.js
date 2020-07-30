7417adba7e2848c1feb917bef15cd64866d11e34
asaldana1108 <asaldana1108@gmail.com>
2020-07-30 18:57:01 -0500

    front end integrated!

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -103,17 +104,29 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-  
+
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The animal is not properly formatted.');
     } else {
         // add animal to json file and animals array in this function
-      const animal = createNewAnimal(req.body, animals);
-      // req.body is where our incoming content will be
-      res.json(animal);
+        const animal = createNewAnimal(req.body, animals);
+        // req.body is where our incoming content will be
+        res.json(animal);
     }
-  });
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
